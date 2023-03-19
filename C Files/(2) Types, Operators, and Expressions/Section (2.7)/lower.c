b755f9d3b065eeff39fee0ad30fc86d1808ce1e0
Cross <akshajrao2004@gmail.com>
2023-03-19 18:20:42 +0530

    Modified files

--- C Files/(2) Types, Operators, and Expressions/Section (2.7)/lower.c
+++ C Files/(2) Types, Operators, and Expressions/Section (2.7)/lower.c
@@ -1,26 +1,22 @@
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
 
 // Two methods for returning a lower case character's value.
 int lower(int c);
 
-
-int main()
-{
+int main() {
   int c;
 
   printf("Enter a character: ");
   c = getchar();
-  
-  printf("Returned output via function lower: %d\n", lower(c));
-  printf("Returned output via standard library function: %d\n", tolower(c));
-  
+
+  printf("Returned output via function lower: %c\n", lower(c));
+  printf("Returned output via standard library function: %c\n", tolower(c));
+
   return 0;
 }
 
-
-int lower(int c)
-{
+int lower(int c) {
   if (c >= 'A' && c <= 'Z')
     return c + 'a' - 'A';
   else
