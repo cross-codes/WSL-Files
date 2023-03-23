2418d3fba6505c479e2c89d0f5bb181ebe952baf
Cross <akshajrao2004@gmail.com>
2023-03-23 21:30:21 +0530

    Something and all

--- C Files/(2) Types, Operators, and Expressions/Section (2.2)/exercise.c
+++ C Files/(2) Types, Operators, and Expressions/Section (2.2)/exercise.c
@@ -1,25 +1,28 @@
-#include <stdio.h>
-#include <limits.h>
 #include <float.h>
+#include <limits.h>
+#include <stdio.h>
 
 /* Exercise (2-1) */
 /* Required : char, short, int, long (both signed and unsigned) */
 
-int main()
-{
-printf("The range of the unsigned char type is: %u to %u\n", 0, UCHAR_MAX);
-printf("The range of the signed char type is: %d to %d\n", CHAR_MIN, SCHAR_MAX);
+int main() {
+  printf("The range of the unsigned char type is: %u to %u\n", 0, UCHAR_MAX);
+  printf("The range of the signed char type is: %d to %d\n", CHAR_MIN,
+         SCHAR_MAX);
 
-printf("\nThe range of the signed short type is: %d to %d\n", SHRT_MIN, SHRT_MAX);
-printf("The range of the unsigned short type is: %u to %u\n", 0, USHRT_MAX);
+  printf("\nThe range of the signed short type is: %d to %d\n", SHRT_MIN,
+         SHRT_MAX);
+  printf("The range of the unsigned short type is: %u to %u\n", 0, USHRT_MAX);
 
-printf("\nThe range of the signed int type is: %d to %d\n", INT_MIN, INT_MAX);
-printf("The range of the unsigned int type is: %d to %u\n", 0, UINT_MAX);
+  printf("\nThe range of the signed int type is: %d to %d\n", INT_MIN, INT_MAX);
+  printf("The range of the unsigned int type is: %d to %u\n", 0, UINT_MAX);
 
-printf("\nThe range of the signed long type is: %ld to %ld\n", LONG_MIN, LONG_MAX);
-printf("The range of the unsigned long type is: %d to %lu\n",0, ULONG_MAX);
+  printf("\nThe range of the signed long type is: %ld to %ld\n", LONG_MIN,
+         LONG_MAX);
+  printf("The range of the unsigned long type is: %d to %lu\n", 0, ULONG_MAX);
 
-printf("\nThe range of the signed float type is: %f to %f\n", FLT_MIN, FLT_MAX);
+  printf("\nThe range of the signed float type is: %f to %f\n", FLT_MIN,
+         FLT_MAX);
 
-return 0;
+  return 0;
 }
---
+++ C Files/(3) Control Flow/Section (3.4)/escape.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void escape(char final[], char initial[]);
+
+int main() {
+  char t[] = "This is a tab space\t";
+  char s[200];
+
+  escape(s, t);
+  printf("%s\n, %s\n", s, t);
+  return 0;
+}
+
+void escape(char s[], char t[]) {
+  int i, j;
+
+  for (i = j = 0; t[i] != '\0'; i++)
+    switch (t[i]) {
+    case '\n':
+      s[j++] = '\\';
+      s[j++] = 'n';
+      break;
+    case '\t':
+      s[j++] = '\\';
+      s[j++] = 't';
+      break;
+    default:
+      s[j++] = t[i];
+      break;
+    }
+  s[j] = '\0';
+}
