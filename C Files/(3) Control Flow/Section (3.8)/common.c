8a00d48e9568478f18e774739e093681f42d762c
Cross <akshajrao2004@gmail.com>
2023-04-01 20:36:05 +0530

    Modified common.c to use goto correctly

--- C Files/(3) Control Flow/Section (3.8)/common.c
+++ C Files/(3) Control Flow/Section (3.8)/common.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 
+char find(int lst1[], int lst2[]);
+
 int main() {
-  int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-  int b[10] = {10, 12, 14, 16, 18, 19, 12};
-  int m, n;
-  for (m = 0; m < 9; ++m) {
-    for (n = 0; n < 7; ++n) {
-      if (a[m] == b[n]) {
-        goto found;
-      } else
-        printf("Not a match\n");
-    }
-  }
-found:
-  printf("Found a match %d, %d, %d\n", a[m - 1], b[n - 1], m - 1);
+  int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  int b[] = {10, 12, 14, 16, 18, 19, 9};
+
+  printf("Found a match? : %c\n", find(a, b));
 
   return 0;
+}
+
+char find(int a[], int b[]) {
+  int i, j;
+
+  for (i = 0; i < 9; ++i)
+    for (j = 0; j < 7; ++j)
+      if (a[i] == b[j])
+        goto found;
+
+  return 'N';
+found:
+  return 'Y';
 }
