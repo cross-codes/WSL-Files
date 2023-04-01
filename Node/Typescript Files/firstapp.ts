08c06ad65547d88400861ae6ed651578c2f61f9c
Cross <akshajrao2004@gmail.com>
2023-04-01 13:29:08 +0530

    New files

--- Node/Typescript Files/firstapp.ts
+++ Node/Typescript Files/firstapp.ts
@@ -2,6 +2,6 @@ function addNumbers(a: number, b: number) {
   return a + b;
 }
 
-const sum: number = addNumbers(10, 15)
+var sum: number = addNumbers(10, 15)
 
 console.log('Sum of the two numbers is: %d', sum);
