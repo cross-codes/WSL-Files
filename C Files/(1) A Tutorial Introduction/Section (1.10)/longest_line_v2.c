7dfea8d5fc2427984b76afa19100f2cbb4f082d9
Cross <akshajrao2004@gmail.com>
2023-04-03 18:09:19 +0530

    Added new files

--- C Files/(1) A Tutorial Introduction/Section (1.10)/longest_line_v2.c
+++ C Files/(1) A Tutorial Introduction/Section (1.10)/longest_line_v2.c
@@ -2,54 +2,49 @@
 
 #define MAXLINE 1000 /* Maximum line size */
 
-int max; /* Maximum line seen so far */
-char line[MAXLINE]; /* Current input line */
+int max;               /* Maximum line seen so far */
+char line[MAXLINE];    /* Current input line */
 char longest[MAXLINE]; /* Longest line here */
 
 int getlines(void);
 void copy(void);
 
 /* Print the longest input line (v2) */
-main()
-{
-    int len;
-    extern int max;
-    extern char longest[];
+int main() {
+  int len;
+  extern int max;
+  extern char longest[];
 
-    max = 0;
-    while ((len = getlines()) > 0)
-        if (len > max) {
-            max = len;
-            copy();
-        }
-    if (max > 0) /* There was a line */
-        printf("%s", longest);
-    return 0;
-}
-
-
-int getlines(void)
-{
-    int c, i;
-    extern char line[];
-
-    for (i = 0; i < MAXLINE - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
-        line[i] = c;
-    if (c == '\n') {
-        line[i] = c;
-        ++i;
+  max = 0;
+  while ((len = getlines()) > 0)
+    if (len > max) {
+      max = len;
+      copy();
     }
-    line[i] = '\0';
-    return i;
+  if (max > 0) /* There was a line */
+    printf("%s", longest);
+  return 0;
 }
 
+int getlines(void) {
+  int c, i;
+  extern char line[];
 
-void copy(void)
-{
-    int i;
-    extern char line[], longest[];
+  for (i = 0; i < MAXLINE - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+    line[i] = c;
+  if (c == '\n') {
+    line[i] = c;
+    ++i;
+  }
+  line[i] = '\0';
+  return i;
+}
 
-    i = 0;
-    while ((longest[i] = line[i]) != '\0')
-        ++i;
+void copy(void) {
+  int i;
+  extern char line[], longest[];
+
+  i = 0;
+  while ((longest[i] = line[i]) != '\0')
+    ++i;
 }
