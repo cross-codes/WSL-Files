b890f37d522f0cae12488ed9e6bd70860ec5e82d
Cross <akshajrao2004@gmail.com>
2023-04-07 17:05:14 +0530

    New node files

---
+++ Node (Udemy)/hello.js
@@ -0,0 +1,2 @@
+console.log('Speaking node from Neovim');
+
--- Node (Udemy)/test.js
+++
@@ -1,5 +0,0 @@
-// take the input from the user
-const number = 69;
-
-const result = Math.sqrt(number);
-console.log(`The square root of ${number} is ${result}`);
