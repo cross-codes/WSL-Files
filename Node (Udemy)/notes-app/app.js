56d32b411ef5ab11fcb3f97ca0247ee9254de8ca
Cross <akshajrao2004@gmail.com>
2023-04-08 12:42:04 +0530

    New files for the notes-app

--- Node (Udemy)/notes-app/app.js
+++ Node (Udemy)/notes-app/app.js
@@ -1,3 +1,4 @@
+const validator = require('validator')
 const add = require('./utils')
 const getNotes = require('./notes')
 
@@ -6,3 +7,5 @@ const sum = add(111, -69)
 console.log(sum)
 const val = getNotes()
 console.log(val)
+console.log(validator.isEmail('gmail.com'))
+console.log(validator.isURL('https://youtube.com'))
