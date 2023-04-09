96f1b5c741467bfb4bcddb9dcd72483a6220c6eb
Cross <akshajrao2004@gmail.com>
2023-04-09 19:42:33 +0530

    Renaming folder

---
+++ Node/notes-app/app.js
@@ -0,0 +1,12 @@
+const chalk = require("chalk");
+const add = require("./utils");
+const getNotes = require("./notes");
+
+const sum = add(111, -69);
+
+console.log(sum);
+
+const val = getNotes();
+console.log(val);
+
+console.log(chalk.green.bold("Success!"));
---
+++ Node/notes-app/utils.js
@@ -0,0 +1,8 @@
+console.log("utlis.js");
+const name = "Cross";
+
+const add = function (x, y) {
+  return x + y;
+};
+
+module.exports = add;
