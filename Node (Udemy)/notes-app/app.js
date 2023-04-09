acc51854bb6320f5032af60641564f30aa4b743c
Cross <akshajrao2004@gmail.com>
2023-04-09 19:24:29 +0530

    New additions

--- Node (Udemy)/notes-app/app.js
+++ Node (Udemy)/notes-app/app.js
@@ -3,9 +3,10 @@ const add = require("./utils");
 const getNotes = require("./notes");
 
 const sum = add(111, -69);
+
 console.log(sum);
 
 const val = getNotes();
 console.log(val);
 
-console.log(chalk.green.bold.inverse("Success!"));
+console.log(chalk.green.bold("Success!"));
