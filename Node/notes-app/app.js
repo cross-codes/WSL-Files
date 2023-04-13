dcdb48bf659ab8f7a61049a0f7d1a52022d2adec
Cross <akshajrao2004@gmail.com>
2023-04-13 23:09:52 +0530

    More progress in node

--- Node/notes-app/app.js
+++ Node/notes-app/app.js
@@ -1,20 +1,43 @@
 const chalk = require("chalk");
-
-const add = require("./utils");
+const yargs = require("yargs");
 const getNotes = require("./notes");
 
-const sum = add(111, -69);
+// Customise yargs version
+yargs.version("1.1.0");
 
-console.log(sum);
+// Creating an add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function () {
+    console.log("Adding a new note!");
+  },
+});
 
-const val = getNotes();
-console.log(val);
+// Creating a remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function () {
+    console.log("Removing the note");
+  },
+});
 
-console.log(chalk.blue.bold("Error!"));
-const command = process.argv[2];
+// Creating a list commmand
+yargs.command({
+  command: "list",
+  describe: "List notes available",
+  handler: function () {
+    console.log("Listing available notes");
+  },
+});
 
-if (command === "add") {
-  console.log("Adding note");
-} else if (command === "remove") {
-  console.log("Removing note");
-}
+// Creating a read command
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  handler: function () {
+    console.log("Reading a note");
+  },
+});
+console.log(yargs.argv);
