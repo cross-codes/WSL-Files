962895180774c317f052f22be9cbd6fd823900a4
Cross <akshajrao2004@gmail.com>
2023-04-15 18:46:41 +0530

    New files from the node course

--- C Files/(5) Pointers and Arrays/Section (5.1)/test.c
+++ C Files/(5) Pointers and Arrays/Section (5.1)/test.c
@@ -4,8 +4,9 @@ int main() {
   int x = 1, y = 2, v[2] = {0, 1};
   int *ip;
 
+  printf("%d\n", *ip);
   ip = &x;
-  printf("%d\n", ip);
+  printf("%d, %d\n", ip, *ip);
   y = *ip;
   printf("%d\n", y);
 }
