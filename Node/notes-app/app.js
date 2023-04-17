e9706eda1535f2d97268a37fb2e1bee8dd0cadf5
Cross <akshajrao2004@gmail.com>
2023-04-17 17:06:53 +0530

    New files from the node course

--- Node/notes-app/app.js
+++ Node/notes-app/app.js
@@ -1,6 +1,6 @@
 const chalk = require("chalk");
 const yargs = require("yargs");
-const getNotes = require("./notes");
+const notes = require("./notes");
 
 // Customise yargs version
 yargs.version("1.1.0");
@@ -22,7 +22,7 @@ yargs.command({
     },
   },
   handler: function(argv) {
-    console.log("Title: " + argv.title + "\n" + "Body: " + argv.body);
+    notes.addNote(argv.title, argv.body);
   },
 });
 
