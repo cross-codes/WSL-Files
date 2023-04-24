f8bb5776fbe56996f62e285d8a3192477e5a928b
Cross <akshajrao2004@gmail.com>
2023-04-24 19:31:45 +0530

    New file

--- C Files/Exercises/test.c
+++ C Files/Exercises/test.c
@@ -1,9 +1,51 @@
 #include <stdio.h>
 
 int main() {
-  int a = {1, 2, 3};
+  int a = 1, b = 4, c = 3, max;
 
-  printf("%d\n", a);
+  max = (a >= b) ? ((a >= c) ? a : c) : ((b >= c) ? b : c);
+
+  printf("%d\n", max);
+
+  int number = 2;
+
+  switch (number) {
+  case 1:
+  case 2:
+  case 3:
+    printf("One, Two or Three\n");
+  case 4:
+  case 5:
+  case 6:
+    printf("Four, Five or Six\n");
+    break;
+  default:
+    printf("Greater than six\n");
+  }
+
+  int rows;
+
+  printf("Enter rows: ");
+  scanf("%d", &rows);
+
+  int starcount, spccount = 0;
+  int count = rows, star = 0, spc;
+  while (count >= 1) {
+    spc = spccount;
+    while (spc > 0) {
+      printf(" ");
+      spc--;
+    }
+    starcount = 2 * count - 1;
+    while (star < starcount) {
+      printf("*");
+      star++;
+    }
+    count--;
+    star = 0;
+    spccount++;
+    printf("\n");
+  }
 
   return 0;
 }
