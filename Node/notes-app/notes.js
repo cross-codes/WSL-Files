9855e8380cc95c77e6d0baf16a61e192b7f6d955
Cross <akshajrao2004@gmail.com>
2023-05-06 20:47:00 +0530

    Update notes app

--- Node/notes-app/notes.js
+++ Node/notes-app/notes.js
@@ -22,12 +22,15 @@ const addNote = (title, body) => {
   const notes = loadNotes();
   const duplicateNote = notes.find((note) => note.title === title);
 
+  debugger;
+
   if (!duplicateNote) {
     notes.push({
       title: title,
       body: body,
     });
 
+    debugger;
     saveNotes(notes);
     console.log(chalk.bgGreen("New note added!"));
   } else {
