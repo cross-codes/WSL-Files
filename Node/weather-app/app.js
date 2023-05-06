2002536b7c8a9cd00428499f1572ae742732101d
Cross <akshajrao2004@gmail.com>
2023-05-06 20:58:41 +0530

    New modifications

--- Node/weather-app/app.js
+++ Node/weather-app/app.js
@@ -4,7 +4,6 @@ const request = require("request");
 _API_KEY = api.apikey;
 const url = "http://api.openweathermap.org/data/2.5/find?q=Pilani&units=imperial&type=accurate&APPID=" + _API_KEY;
 
-request({ url: url }, (error, response) => {
-  const data = (JSON.parse(response.body)).list;
-  console.log(data);
+request({ url: url, json: true }, (error, response) => {
+  console.log(response.body.list[0]);
 });
