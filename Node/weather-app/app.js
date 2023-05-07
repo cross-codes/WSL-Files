08f1e541723c376b77bbbe2dedd2551fc91de446
Cross <akshajrao2004@gmail.com>
2023-05-07 11:46:39 +0530

    Minor changes

--- Node/weather-app/app.js
+++ Node/weather-app/app.js
@@ -2,7 +2,7 @@ const api = require("./.secrets.js");
 const request = require("request");
 
 _API_KEY = api.apikey;
-const url = "http://api.openweathermap.org/data/2.5/find?q=Pilani&units=imperial&type=accurate&APPID=" + _API_KEY;
+const url = "http://api.openweathermap.org/data/2.5/find?q=Mangalore&units=metric&type=accurate&APPID=" + _API_KEY;
 
 request({ url: url, json: true }, (error, response) => {
   console.log(response.body.list[0]);
