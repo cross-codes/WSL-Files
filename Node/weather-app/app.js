c95062be6b8a5306ab1ae545d5fb5da274236cf3
Akshaj Rao <akshajrao2004@gmail.com>
2023-05-08 16:18:25 +0530

    New updates from the node course

--- Node/weather-app/app.js
+++ Node/weather-app/app.js
@@ -1,36 +1,39 @@
 const api = require("./.env");
-const request = require("request");
+const geocode = require("./utils/geocode");
 
 const _API_KEY = api.apikey;
 const url_w =
   "http://api.openweathermap.org/data/2.5/weather?lat=51.509865&lon=-0.118092&units=metric&type=accurate&appid="
   + _API_KEY;
 
-const url_g = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid=" + _API_KEY;
-
-request({ url: url_w, json: true }, (error, response) => {
-  if (error) {
-    console.log("Unable to connect to the weather service!");
-  } else if (response.body.message) {
-    console.log("Unable to find given location");
-  } else {
-    const main_body_w = response.body;
-    console.log(
-      "Description: " + main_body_w.weather[0].description + ". It is currently " + main_body_w.main.temp
-        + " degrees out. The humidity is " + main_body_w.main.humidity + "%.",
-    );
-  }
+geocode.geocode("Reykjavik", (error, data) => {
+  console.log("Error", error);
+  console.log("Data", data);
 });
 
-request({ url: url_g, json: true }, (error, response) => {
-  if (error) {
-    console.log("Unable to connect to the geolocation service!");
-  } else if (response.body.length === 0) {
-    console.log("Given location not found!");
-  } else {
-    const main_body_g = response.body[0];
-    const latitude = main_body_g.lat;
-    const longitude = main_body_g.lon;
-    console.log("The latitute is: " + latitude + " and the longitude is: " + longitude + ".");
-  }
-});
+// request({ url: url_w, json: true }, (error, response) => {
+//   if (error) {
+//     console.log("Unable to connect to the weather service!");
+//   } else if (response.body.message) {
+//     console.log("Unable to find given location");
+//   } else {
+//     const main_body_w = response.body;
+//     console.log(
+//       "Description: " + main_body_w.weather[0].description + ". It is currently " + main_body_w.main.temp
+//         + " degrees out. The humidity is " + main_body_w.main.humidity + "%.",
+//     );
+//   }
+// });
+//
+// request({ url: url_g, json: true }, (error, response) => {
+//   if (error) {
+//     console.log("Unable to connect to the geolocation service!");
+//   } else if (response.body.length === 0) {
+//     console.log("Given location not found!");
+//   } else {
+//     const main_body_g = response.body[0];
+//     const latitude = main_body_g.lat;
+//     const longitude = main_body_g.lon;
+//     console.log("The latitute is: " + latitude + " and the longitude is: " + longitude + ".");
+//   }
+// });
---
+++ Node/weather-app/utils/geocode.js
@@ -0,0 +1,26 @@
+const request = require("request");
+const api = require("../.env");
+
+const _API_KEY = api.apikey;
+const geocode = (address, callback) => {
+  const url = "http://api.openweathermap.org/geo/1.0/direct?q=" + encodeURIComponent(address) + "&limit=1&appid="
+    + _API_KEY;
+
+  request({ url: url, json: true }, (error, response) => {
+    if (error) {
+      callback("Unable to connect to the location service", undefined);
+    } else if (response.body.message) {
+      callback("Unable to find given location, try another search", undefined);
+    } else {
+      callback(undefined, {
+        latitude: response.body[0].lat,
+        longitude: response.body[0].lon,
+        location: response.body[0].name,
+      });
+    }
+  });
+};
+
+module.exports = {
+  geocode: geocode,
+};
