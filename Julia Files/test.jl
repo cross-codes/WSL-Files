be203a824d7561488820f14ac3c925d9e414c6ce
Akshaj Rao <cross@inspiron>
2023-05-23 23:30:25 +0530

    Modified test.jl to test lsp

--- Julia Files/test.jl
+++ Julia Files/test.jl
@@ -1,14 +1,31 @@
-function mandelbrot(a)
-    z = 0
-    for i = 1:50
-        z = z^2 + a
+# This is Julia code; it will not run in MATLAB or Python.
+using Plots, StatsBase, MAT
+
+function crosscorr(x, y)
+    lx, rxx, lags = length(x), AbstractFloat[], Integer[]
+    x = vcat(zeros(lx - 1), x, zeros(lx - 1))
+    lags_ranges = [(k - lx, k:k+lx-1) for k = 1:2*lx-1]
+
+    foreach(lags_ranges) do (lag, range)
+        push!(rxx, mean(x[range] .* y))
+        push!(lags, lag)
     end
-    return z
+    return rxx, lags
 end
 
-for y = 1.0:-0.05:-1.0
-    for x = -2.0:0.0315:0.5
-        abs(mandelbrot(complex(x, y))) < 2 ? print("*") : print(" ")
-    end
-    println()
+N, fs = length(eeg), 50  # Length, sampling frequency of data
+t = (0:N-1) / fs           # Time vector from 0 (approx.) to 2 sec.
+freqs = 1:0.5:25
+refs = [cos.(2π .* f .* t) for f = freqs]       # Reference signals
+result = zip(refs, map(x -> crosscorr(eeg, x), refs))
+
+max_corr, max_corr_M = [], []
+foreach(result) do (ref, (rxx, lags))
+    push!(max_corr, maximum(rxx))
+    push!(max_corr_M, maximum(crosscov(eeg, ref, lags, demean=false)))
 end
+
+xlabel = "Reference Signal Frequency (Hz)"
+ylabel = "Max correlation"
+plot(freqs, max_corr, label="Manual", xlabel=xlabel, ylabel=ylabel)
+plot!(freqs, max_corr_M, markershape=:circle, ls=:dot, label="StatsBase")
