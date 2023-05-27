a67166256378a7b8b696a289512495077ea8123a
Akshaj Rao <akshajrao2004@gmail.com>
2023-05-27 15:24:46 +0530

    Add new files

---
+++ C Files/(6) Structures/Section (6.3)/arrays.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#pragma pack(4)
+
+int main() {
+  struct key {
+    char *word;
+    int count;
+  } keytab[] = {
+      {"auto", 0},
+      {"break", 0},
+      {"case", 0},
+  };
+
+  printf("%p\n", keytab[0].word);
+  return 0;
+}
