8e63fa597956b5e25fedbcff8d5134c10ebb4790
Akshaj Rao <akshajrao2004@gmail.com>
2023-05-29 21:43:40 +0530

    New stuff from the node course

--- Node/task-manager/mongodb.js
+++ Node/task-manager/mongodb.js
@@ -1,5 +1,31 @@
 // Crud operations - Create, Read, Update and Delete operations
 
+// db.collection("users").insertOne({
+//   name: "Prashanth Rao",
+//   age: 60,
+// }, (error, result) => {
+//   if (error) {
+//     return console.log("Unable to insert user");
+//   }
+//   console.log(result);
+// });
+// db.collection("users").insertMany([
+//   {
+//     name: "Jyothi",
+//     age: 47,
+//   },
+//   {
+//     name: "Tavish",
+//     age: 19,
+//   },
+// ], (error, result) => {
+//   if (error) {
+//     console.log("Unable to insert documents");
+//   } else {
+//     console.log(result);
+//   }
+// });
+
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
@@ -12,8 +38,25 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
   }
 
   const db = client.db(databaseName);
-  db.collection("users").insertOne({
-    name: "Mark",
-    age: 30,
+
+  db.collection("tasks").insertMany([
+    {
+      description: "This is a test task",
+      completed: true,
+    },
+    {
+      description: "Configure neovim to be the best editor in the world",
+      completed: true,
+    },
+    {
+      description: "Go to sleep",
+      completed: false,
+    },
+  ], (error, response) => {
+    if (error) {
+      console.log("Unable to insert documents");
+    } else {
+      console.log(response);
+    }
   });
 });
