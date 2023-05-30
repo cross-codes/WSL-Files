6268bcfcd9f1f5917a03ce9baa046075e6499d4e
Akshaj Rao <akshajrao2004@gmail.com>
2023-05-30 18:54:37 +0530

    Move files

---
+++ Competitve Coding/Leetcode/2656.c
@@ -0,0 +1,15 @@
+int maximizeSum(int *nums, int numsSize, int k) {
+  int ans = 0;
+  int gap, i, j, temp;
+
+  for (gap = numsSize / 2; gap > 0; gap /= 2)
+    for (i = gap; i < numsSize; i++)
+      for (j = i - gap; j >= 0 && nums[j] > nums[j + gap]; j -= gap) {
+        temp = nums[j];
+        nums[j] = nums[j + gap];
+        nums[j + gap] = temp;
+      }
+  ans += nums[numsSize - 1] * k + (k - 1) * k / 2;
+
+  return ans;
+}
