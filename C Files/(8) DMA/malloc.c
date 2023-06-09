d921aa155a2eef02893ad7439ff44853822b56bc
Akshaj Rao <akshajrao2004@gmail.com>
2023-06-09 12:51:56 +0530

    Update files

--- C Files/(8) DMA/malloc.c
+++ C Files/(8) DMA/malloc.c
@@ -6,18 +6,21 @@ int main() {
   short *p1;
   int *p2;
   float *p3;
+  int *p4;
 
   p1 = (short *)malloc(sizeof(short));
-  p2 = (int *)malloc(sizeof(int));
+  p2 = (int *)malloc(6000 * sizeof(int));
   p3 = (float *)malloc(sizeof(float));
+  p4 = (int *)calloc(4, sizeof(int));
 
   *p1 = 256;
-  *p2 = 100;
   *p3 = 123.456;
 
   printf("p1 is %hd\n", *p1);
   printf("p2 is %d\n", *p2);
   printf("p3 is %f\n", *p3);
+  printf("sizeof(p2) is %lu", sizeof(p2));
+  printf("sizeof(p4) is %lu", sizeof(p4));
 
   return 0;
 }
