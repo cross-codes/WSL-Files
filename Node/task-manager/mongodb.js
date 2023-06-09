ac6a6e963bb86567f9e7254773373f2beef1df3b
Akshaj Rao <akshajrao2004@gmail.com>
2023-06-09 23:23:58 +0530

    New stuff from the node course

--- Node/task-manager/mongodb.js
+++ Node/task-manager/mongodb.js
@@ -2,9 +2,8 @@
 
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
-const ObjectID = mongodb.ObjectId;
+const ObjectId = mongodb.ObjectId;
 
-const a = 2;
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
 
