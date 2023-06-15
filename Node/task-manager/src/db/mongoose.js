a78495e92aaa5df158191d80d7797500f7094c4c
Akshaj Rao <akshajrao2004@gmail.com>
2023-06-16 00:14:36 +0530

    New stuff

--- Node/task-manager/src/db/mongoose.js
+++ Node/task-manager/src/db/mongoose.js
@@ -1,8 +1,37 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
   useCreateIndex: true,
+});
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validator(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    },
+  },
 });
 
 const Task = mongoose.model("Task", {
@@ -12,15 +41,4 @@ const Task = mongoose.model("Task", {
   completed: {
     type: Boolean,
   },
-});
-
-const test = new Task({
-  description: "This is a test task",
-  completed: true,
-});
-
-test.save().then(() => {
-  console.log(test);
-}).catch((error) => {
-  console.log("Error ", error);
 });
