f1ea47567822151333a5bb1ef252586b850ba1c0
Akshaj Rao <akshajrao2004@gmail.com>
2023-06-19 00:25:17 +0530

    Switch to eslint config

--- Node/task-manager/src/db/mongoose.js
+++ Node/task-manager/src/db/mongoose.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
-const validator = require("validator");
+import mongoose from "mongoose";
+import validator from "validator";
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
@@ -32,13 +32,45 @@ const User = mongoose.model("User", {
       }
     },
   },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    min: [7, "Password length must be atleast 7 characters"],
+    validator(value) {
+      if ((value.toLowerCase().includes("password"))) {
+        throw new Error("You can do better than that");
+      }
+    },
+  },
 });
 
 const Task = mongoose.model("Task", {
   description: {
     type: String,
+    trim: true,
+    required: true,
   },
   completed: {
     type: Boolean,
+    required: false,
+    default: false,
   },
+});
+
+const user = new User({
+  name: "Scaramouche",
+  age: 500,
+  email: "scaramouche_06@fatui.org",
+  password: "shoukinokami",
+});
+
+const task = new Task({
+  description: "This task passes validation",
+});
+
+task.save().then(() => {
+  console.log(task);
+}).catch(error => {
+  console.log(`Error: ${error}`);
 });
