9b96420631d7c7003ba2c42db58cb7fcee360c69
Akshaj Rao <akshajrao2004@gmail.com>
2023-06-18 17:22:14 +0530

    Add a .xo-config.js

---
+++ Node/task-manager/.xo-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  rules: {
+    indent: ["error", 2],
+    "no-mixed-spaces-and-tabs": "off",
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+  },
+  options: {
+    space: true,
+  },
+};
