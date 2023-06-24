bbc8c7b54b8edd27e590b19796403abe29ac9655
Akshaj Rao <akshajrao2004@gmail.com>
2023-06-24 15:49:11 +0530

    Update script files

--- C Files/Exercises/makeup/myScript.sh
+++ C Files/Exercises/makeup/myScript.sh
@@ -1,4 +1,15 @@
-rm exe && rm *.o
+#!/bin/bash
+
+files=$(find . -name "*.o")
+
+if [ -f "exe" ]; then
+	rm exe
+fi
+
+if [ -n "$files" ]; then
+	rm *.o
+fi
+
 gcc -c defs.c && gcc -c main.c
 gcc -o exe defs.o main.o
 ./exe
