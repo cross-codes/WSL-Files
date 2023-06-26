312f07f95e77387301f9df2312015212b57b4494
Akshaj Rao <akshajrao2004@gmail.com>
2023-06-26 21:48:25 +0530

    New stufff

--- Node/task-manager/playground/promise-chaining-2.js
+++ Node/task-manager/playground/promise-chaining-2.js
@@ -1,11 +1,24 @@
 import "../src/db/mongoose.js";
 import Task from "../src/models/tasks.js";
 
-Task.findByIdAndRemove("64872f835bef0b5deba2b72f").then((task) => {
-  console.log(task);
-  return Task.countDocuments({ completed: false });
-}).then((result) => {
-  console.log(result);
+// Task.findByIdAndRemove("64872f835bef0b5deba2b72f").then((task) => {
+//   console.log(task);
+//   return Task.countDocuments({ completed: false });
+// }).then((result) => {
+//   console.log(result);
+// }).catch((e) => {
+//   console.log(e);
+// });
+
+const deleteTaskAndCount = async (id) => {
+  const task = await Task.findByIdAndRemove(id);
+  const count = await Task.countDocuments({ completed: false });
+
+  return count;
+};
+
+deleteTaskAndCount("648f47a37556ec63921c5438").then((count) => {
+  console.log(count);
 }).catch((e) => {
   console.log(e);
 });
--- Node/task-manager/playground/promise-chaining.js
+++ Node/task-manager/playground/promise-chaining.js
@@ -1,11 +1,24 @@
 import "../src/db/mongoose.js";
 import User from "../src/models/users.js";
 
-User.findByIdAndUpdate("64972aad3aed9171235db43b", { age: 20 }).then((user) => {
-  console.log(user);
-  return User.countDocuments({ age: 20 });
-}).then((result) => {
-  console.log(result);
+// User.findByIdAndUpdate("64972aad3aed9171235db43b", { age: 20 }).then((user) => {
+//   console.log(user);
+//   return User.countDocuments({ age: 20 });
+// }).then((result) => {
+//   console.log(result);
+// }).catch((e) => {
+//   console.log(e);
+// });
+
+const updateAgeAndCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id, { age });
+  const count = await User.countDocuments({ age });
+
+  return count;
+};
+
+updateAgeAndCount("64972aad3aed9171235db43b", 2).then((count) => {
+  console.log(count);
 }).catch((e) => {
   console.log(e);
 });
