ea5e36542914ea2afadd862932336eb8b86164e8
Akshaj Rao <akshajrao2004@gmail.com>
2023-07-16 23:31:12 +0530

    Minor updates to the JWT decoder client

--- Node/jwt-client/src/app.js
+++ Node/jwt-client/src/app.js
@@ -1,3 +1,21 @@
 import express from "express";
+import hbs from "hbs";
+import path from "path";
 
 const app = express();
+
+const viewPath = path.join(__dirname, "../templates/views");
+const publicPath = path.join(__dirname, "../public");
+const partialPath = path.join(__dirname, "../templates/partials");
+
+app.set("view engine", "hbs");
+app.set("views", viewPath);
+app.use(express.static(publicPath));
+hbs.registerPartials(partialPath);
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "JWT Decoder Client",
+    name: "cross-codes",
+  });
+});
--- Node/jwt-client/src/utils/decoder.js
+++ Node/jwt-client/src/utils/decoder.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+// Examples
+// "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoic3NzcyIsIklzc3VlciI6ImFzcyIsIlVzZXJuYW1lIjoiYWFhYSIsImV4cCI6MTY4OTUyOTAyMiwiaWF0IjoxNjg5NTI5MDIyfQ.6S4cRSzDF0JC-qmh1SUX_XvmyKgD5_m1Md5-yqlvzhs"
+
+import jwt_decode from "jwt-decode";
+
+const token =
+  "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoic3NzcyIsIklzc3VlciI6ImFzcyIsIlVzZXJuYW1lIjoiYWFhYSIsImV4cCI6MTY4OTUyOTAyMiwiaWF0IjoxNjg5NTI5MDIyfQ.6S4cRSzDF0JC-qmh1SUX_XvmyKgD5_m1Md5-yqlvzhs";
+const decoded = jwt_decode(token);
+
+console.log(decoded);
