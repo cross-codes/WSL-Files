c35cf0a06371e792da5871274bc83259fd4e52e5
Akshaj Rao <akshajrao2004@gmail.com>
2023-08-15 23:57:17 +0530

    New test cases from the node course

--- Node/task-manager/tests/user.test.js
+++ Node/task-manager/tests/user.test.js
@@ -91,3 +91,41 @@ test("Should not delete account if unauthorized", async () => {
     .send()
     .expect(401);
 });
+
+test("Should upload avatar image", async () => {
+  await request(app)
+    .post("/users/me/avatar")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .attach("avatar", "tests/fixtures/pokemon.png")
+    .expect(200);
+
+  const user = await User.findById(userOneID);
+  expect(user.avatar).toEqual(expect.any(Buffer));
+});
+
+test("Should update valid user fields", async () => {
+  const newUser = {
+    name: "Maddison",
+    email: "maddison@example.com",
+    password: "maddison!!__",
+  };
+  await request(app)
+    .patch("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send(newUser).expect(200);
+
+  const user = await User.findById(userOneID);
+  expect(user.name).toBe(newUser.name);
+  expect(user.email).toBe(newUser.email);
+});
+
+test("Should not update invalid user fields", async () => {
+  const badUser = {
+    name: "Sarah",
+    location: "New Jersey",
+  };
+  await request(app)
+    .patch("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send(badUser).expect(400);
+});
