8ad2d777c43dc77e2adb6b7471a46ad7359670c2
Akshaj Rao <akshajrao2004@gmail.com>
2023-08-28 00:04:48 +0530

    New stuff from the node course

--- Node/chat-app/public/js/chat.js
+++ Node/chat-app/public/js/chat.js
@@ -12,3 +12,21 @@ document.querySelector("#messageForm").addEventListener("submit", (e) => {
 socket.on("sendMessage", (message) => {
   console.log(message);
 });
+
+document.querySelector("#send-location").addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported by your browser");
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    const location = {};
+    location.latitude = position.coords.latitude;
+    location.longitude = position.coords.longitude;
+
+    socket.emit("sendLocation", `https://google.com/maps?q=${location.latitude},${location.longitude}`);
+  });
+});
+
+socket.on("sendLocation", (locationString) => {
+  console.log(locationString);
+});
