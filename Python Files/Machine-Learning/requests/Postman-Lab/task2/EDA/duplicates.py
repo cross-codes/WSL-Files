72739c25d9d880101d8bf6b4e6a80c5d63adb1b1
Akshaj Rao <akshajrao2004@gmail.com>
2023-09-16 17:01:08 +0530

    Clear space

--- Python Files/Machine-Learning/requests/Postman-Lab/task2/EDA/duplicates.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("../original.csv")
-print(df[df.duplicated].shape)
-print(df.describe())
-
-df = df.drop_duplicates(keep="first")
-df.to_csv("../processed.csv", index=False)
-print(df.describe())
