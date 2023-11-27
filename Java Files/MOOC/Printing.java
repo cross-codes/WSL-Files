a872329f902bfb40e3ac2e023cf3fdf956ea626d
Akshaj Rao <akshajrao2004@gmail.com>
2023-11-27 16:23:25 +0530

    New shit

---
+++ Java Files/MOOC/Printing.java
@@ -0,0 +1,7 @@
+public class Printing {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    System.out.println("Ada Lovelace");
+    System.out.println("sysout is my snippet shortcut");
+  }
+}
