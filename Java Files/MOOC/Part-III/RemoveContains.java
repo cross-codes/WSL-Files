b0cf5417d9fd7a81fde15df1a27885206175414a
Akshaj Rao <akshajrao2004@gmail.com>
2024-01-09 01:07:53 +0530

    New shit

--- Java Files/MOOC/Part-III/RemoveContains.java
+++ Java Files/MOOC/Part-III/RemoveContains.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class Remove {
+public class RemoveContains {
   public static void main(String[] args) {
     ArrayList<String> list = new ArrayList<>();
 
@@ -12,5 +12,10 @@ public class Remove {
 
     System.out.println("Index 0 so the first value: " + list.get(0));
     System.out.println("Index 1 so the second value: " + list.get(1));
+
+    boolean found = list.contains("Second");
+    if (found) {
+      System.out.println("Second was found");
+    }
   }
 }
