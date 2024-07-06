d747d994606435045f4cc02e1755ec578b014a43
Akshaj Rao <akshajrao2004@gmail.com>
2024-07-06 17:19:18 +0530

    New solutions

---
+++ Competitive coding/Codeforces/Practice/1541/B/Solution.java
@@ -0,0 +1,84 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Locale;
+import java.util.StringTokenizer;
+
+public class Solution implements Runnable {
+
+  BufferedReader in;
+  PrintWriter out;
+  StringTokenizer tok = new StringTokenizer("");
+
+  public static void main(String[] args) {
+    new Thread(null, new Solution(), "", 256 * (1L << 20)).start();
+  }
+
+  public void run() {
+    try {
+      long t1 = 0;
+      boolean isJudge = System.getProperty("ONLINE_JUDGE") != null;
+      if (isJudge) {
+        in = new BufferedReader(new InputStreamReader(System.in), 32768);
+        out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out), 32768));
+      } else {
+        t1 = System.currentTimeMillis();
+        in = new BufferedReader(new FileReader("input.txt"), 32768);
+        out = new PrintWriter(new BufferedWriter(new FileWriter("output.txt"), 32768));
+      }
+      Locale.setDefault(Locale.US);
+      solve();
+      in.close();
+      out.close();
+      if (!isJudge) {
+        long t2 = System.currentTimeMillis();
+        System.err.println("Time = " + (t2 - t1) + " ms");
+      }
+    } catch (Throwable t) {
+      t.printStackTrace(System.err);
+      System.exit(-1);
+    }
+  }
+
+  String readString() throws IOException {
+    while (!tok.hasMoreTokens()) {
+      tok = new StringTokenizer(in.readLine());
+    }
+    return tok.nextToken();
+  }
+
+  int readInt() throws IOException {
+    return Integer.parseInt(readString());
+  }
+
+  long readLong() throws IOException {
+    return Long.parseLong(readString());
+  }
+
+  double readDouble() throws IOException {
+    return Double.parseDouble(readString());
+  }
+
+  void solve() throws IOException {
+    int t = readInt();
+    while (t-- > 0) {
+      int n = readInt();
+      int count = 0;
+      int[] indices = new int[2 * n + 1];
+      for (int j = 1; j <= n; ++j) {
+        int aj = readInt();
+        for (int ai = 1; ai * aj < 2 * j; ++ai) {
+          int k = indices[ai];
+          if (k > 0 && ai * aj == k + j) count++;
+        }
+        indices[aj] = j;
+      }
+      out.println(count);
+    }
+  }
+}
